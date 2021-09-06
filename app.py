e0070b07a656b6de9e4d83b0a52ecaf6033dc9a7
Jtatibana <88391087+Jtatibana@users.noreply.github.com>
2021-09-07 08:13:52 +0900

    login_jissou

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from datetime import datetime, date
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, Response, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin
+from collections import defaultdict
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
@@ -13,7 +15,73 @@ class Post(db.Model):
     due = db.Column(db.DateTime, nullable=True)
 
 
-@app.route('/', methods={'POST', 'GET'})
+login_manager = LoginManager()
+login_manager.init_app(app)
+app.config['SECRET_KEY'] = "secret"
+
+class User(UserMixin):
+    def __init__(self, id, name, password):
+        self.id = id
+        self.name = name
+        self.password = password
+
+# ログイン用ユーザー作成
+users = {
+    1: User(1, "user01", "password"),
+    2: User(2, "user02", "password")
+}
+
+# ユーザーチェックに使用する辞書作成
+nested_dict = lambda: defaultdict(nested_dict)
+user_check = nested_dict()
+for i in users.values():
+    user_check[i.name]["password"] = i.password
+    user_check[i.name]["id"] = i.id
+
+@login_manager.user_loader
+def load_user(user_id):
+    return users.get(int(user_id))
+
+@app.route('/')
+def home():
+    return Response("home: <a href='/login/'>Login</a> <a href='/login_top'>index画面</a> <a href='/logout/'>Logout</a>")
+
+# ログインしないと表示されないパス
+@app.route('/protected/')
+@login_required
+def protected():
+    return Response('''
+    protected<br />
+    <a href="/logout/">logout</a>
+    ''')
+
+# ログインパス
+@app.route('/login/', methods=["GET", "POST"])
+def login():
+    if(request.method == "POST"):
+        # ユーザーチェック
+        if(request.form["username"] in user_check and request.form["password"] == user_check[request.form["username"]]["password"]):
+            # ユーザーが存在した場合はログイン
+            login_user(users.get(user_check[request.form["username"]]["id"]))
+            return redirect('/login_top')
+        else:
+            return abort(401)
+    else:
+        return render_template("login.html")
+
+# ログアウトパス
+@app.route('/logout/')
+@login_required
+def logout():
+    logout_user()
+    return Response('''
+    logout success!<br />
+    <a href="/login/">login</a>
+    ''')
+
+
+@app.route('/login_top', methods={'POST', 'GET'})
+@login_required
 def index():
     if request.method == 'GET':
         posts = Post.query.order_by(Post.due).all()
@@ -28,9 +96,10 @@ def index():
 
         db.session.add(new_post)
         db.session.commit()
-        return redirect('/')
+        return redirect('/login_top')
 
 @app.route('/create')
+@login_required
 def create():
     return render_template('create.html')
 
@@ -40,6 +109,7 @@ def read(id):
     return render_template('detail.html', post=post)
 
 @app.route('/update/<int:id>', methods={'GET', 'POST'})
+@login_required
 def update(id):
     post = Post.query.get(id)
     if request.method == 'GET':
@@ -51,14 +121,15 @@ def update(id):
         post.due = datetime.strptime(request.form.get('due'), '%Y-%m-%d')
 
         db.session.commit()
-        return redirect('/')
+        return redirect('/login_top')
 
 @app.route('/delete/<int:id>')
+@login_required
 def delete(id):
     post = Post.query.get(id)
     db.session.delete(post)
     db.session.commit()
-    return redirect('/')
+    return redirect('/login_top')
 
 
 if __name__ == "__main__":
